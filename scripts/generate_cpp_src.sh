1d3b1dab6a9252fdaf3924c996908fb53e013516
dongliang Qin <dongliang0908@126.com>
2020-03-05 14:35:46 +0800

    write the script: generate_cpp_src.sh

--- scripts/generate_cpp_src.sh
+++ scripts/generate_cpp_src.sh
@@ -18,10 +18,22 @@ CMAKE_SOURCE_DIR=$(dirname $(greadlink -f $0))
 CMAKE_SOURCE_DIR=$(pwd)
 cd ${CMAKE_SOURCE_DIR}
 CPP_SRC_DIR=${CMAKE_SOURCE_DIR}/include/solutions
+TEST_SRC_DIR=${CMAKE_SOURCE_DIR}/tests
 
 if [ ! -n "$1" ]; then
     echo "Please input problem number."
     exit 1
 else
     PROBLEM_NUM=$1
-    leetcode show ${PROBLEM_NUM} -gx -l cpp -o 
+    leetcode show ${PROBLEM_NUM} -gx -l cpp -o ${CPP_SRC_DIR}
+    NEW_CPP_FILE=$(ls ${CMAKE_SOURCE_DIR}/include/solutions | grep ${PROBLEM_NUM})
+    PROBLEM_DESC = $(echo ${NEW_CPP_FILE} | sed 's/.cpp//g')
+    NEW_HPP_FILE=$(echo ${NEW_CPP_FILE} | sed 's/.cpp/.hpp/g')
+fi
+NEW_TEST_FILE=test${NEW_CPP_FILE}
+cp -f ${CMAKE_SOURCE_DIR}/templates/test_template.cpp ${TEST_SRC_DIR}/${NEW_TEST_FILE}
+
+sed -i "s/placeholder/$(expr ${PROBLEM_DESC})/g" \
+    ${TEST_SRC_DIR}/${NEW_TEST_FILE}
+
+
---
+++ templates/test_template.cpp
@@ -0,0 +1,8 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+#include <vector>
+#include <solutions/placeholder.hpp>
+
+TEST_CASE("test placeholder", "[placeholder]") {
+  REQUIRE(1==0);
+}
