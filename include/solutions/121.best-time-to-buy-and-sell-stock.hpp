be3f93bc3f63bc93d4484216b3e8ea7dcca693e2
Georg <qin.dongliang.nudt@gmail.com>
2020-03-10 21:36:44 +0800

    problem 121 solved and accepted.

---
+++ include/solutions/121.best-time-to-buy-and-sell-stock.hpp
@@ -0,0 +1,71 @@
+/*
+ * @lc app=leetcode id=121 lang=cpp
+ *
+ * [121] Best Time to Buy and Sell Stock
+ *
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+ *
+ * algorithms
+ * Easy (48.72%)
+ * Likes:    4083
+ * Dislikes: 185
+ * Total Accepted:    737.9K
+ * Total Submissions: 1.5M
+ * Testcase Example:  '[7,1,5,3,6,4]'
+ *
+ * Say you have an array for which the i^th element is the price of a given
+ * stock on day i.
+ * 
+ * If you were only permitted to complete at most one transaction (i.e., buy
+ * one and sell one share of the stock), design an algorithm to find the
+ * maximum profit.
+ * 
+ * Note that you cannot sell a stock before you buy one.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [7,1,5,3,6,4]
+ * Output: 5
+ * Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit
+ * = 6-1 = 5.
+ * Not 7-1 = 6, as selling price needs to be larger than buying price.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [7,6,4,3,1]
+ * Output: 0
+ * Explanation: In this case, no transaction is done, i.e. max profit = 0.
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maxProfit(std::vector<int>& prices) {
+        int len = prices.size();
+        if (len == 0) return 0;
+        int min_price = prices[0];
+        int maxProfit = 0;
+        for (auto i = 1; i < len; ++i) {
+            if (prices[i] > prices[i-1]) {
+                maxProfit = max(maxProfit, prices[i]-min_price);
+            } else {
+                min_price = min(prices[i], min_price);
+            }
+        }
+        return maxProfit;
+    }
+ private:
+    int max(int a, int b) {
+        return (b > a) ? b : a;
+    }
+
+    int min(int a, int b) {
+        return (a < b) ? a : b;
+    }
+};
+// @lc code=end
---
+++ tests/test102.binary-tree-level-order-traversal.cpp
@@ -0,0 +1,8 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+#include <vector>
+#include <solutions/102.binary-tree-level-order-traversal.hpp>
+
+TEST_CASE("test 102.binary-tree-level-order-traversal", "[102.binary-tree-level-order-traversal]") {
+  REQUIRE(1==0);
+}
