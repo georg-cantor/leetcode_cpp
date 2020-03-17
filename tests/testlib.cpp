cb9fb89b9db03b7bd472f3b21a76e5b8e9caf8f6
Georg <qin.dongliang.nudt@gmail.com>
2020-03-17 11:03:53 +0800

    add test case for testlib and the build_from_level_order should be fixed.

--- tests/testlib.cpp
+++ tests/testlib.cpp
@@ -11,6 +11,7 @@ TEST_CASE("Factorials are computed", "[factorial]") {
     std::vector<int> in1{1,2,3,-1,-1,4,5};
     std::vector<int> in2{1,2,3,-1,-1,4,5,-1,-1,6,7,8,9};
     std::vector<int> in3{1,2};
+    std::vector<int> in4{1,-1,3,2};
     REQUIRE(Factorial(1) == 1);
     TreeNode* tree = build_from_level_order(in1);
     show(tree, 0);
@@ -27,6 +28,10 @@ TEST_CASE("Factorials are computed", "[factorial]") {
     ListNode* testList = build_from_vec(in1);
     printList(testList);
     destroyList(testList);
+    std::cout << "============================================" << std::endl;
+    tree = build_from_level_order(in4);
+    show(tree, 0);
+    destroyTree(tree);
     /*
     REQUIRE(Factorial(1) == 1);
     REQUIRE(Factorial(2) == 2);
