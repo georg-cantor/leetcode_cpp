99d4c7e3c7babca6ad76b53035d77c88aa5274e4
dongliang Qin <dongliang0908@126.com>
2020-06-26 23:49:24 +0800

    problem 129.sum-root-to-leaf-numbers solved and accepted

---
+++ tests/test129.sum-root-to-leaf-numbers.cpp
@@ -0,0 +1,8 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+#include <vector>
+#include <solutions/129.sum-root-to-leaf-numbers.hpp>
+
+TEST_CASE("test 129.sum-root-to-leaf-numbers", "[129.sum-root-to-leaf-numbers]") {
+  REQUIRE(1==0);
+}
